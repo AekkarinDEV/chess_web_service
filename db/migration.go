7de42dd279dd225898cb70f442d801f7d6fbdf7f
AekkarinDEV <ezio4227@gmail.com>
2025-07-14 19:56:13 +0700

    fix migration error

--- db/migration.go
+++ db/migration.go
@@ -11,7 +11,7 @@ func MigrateDb() error {
 		id VARCHAR(255) PRIMARY KEY,
   		username VARCHAR(255) NOT NULL UNIQUE,
   		password VARCHAR(255) NOT NULL,
-  		email VARCHAR(255) NOT NULL UNIQUE
+  		email VARCHAR(255) NOT NULL UNIQUE,
 		refresh_token TEXT
 	)
 	`
